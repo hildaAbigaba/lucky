82fa24232240ffae245d8d75bec8286edf3aaf10
hildaAbigaba <abigabahilda@gmail.com>
2025-07-08 16:00:16 +0300

    Added work

--- home/views.py
+++ home/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
+students = []
+
+def HomePage(request):
+    if request.method == 'POST':
+        #Creating a variable in which we store the contents that we received from the POST method of the form in index.html
+        form_data = request.POST
+        #Creating a variable name that will capture the field names {student_name} from the form
+        name = form_data['student_name']
+        #Creating a variable name that will capture the second field {lucky_number} from the form
+        luck_number = form_data['lukcy_number']
+
+        
+        #Below indicates what we want to do with the data received from the form.
+        new_student = {
+            'name': name,
+            'lucky_number':luck_number,
+        }
+        students.append(new_student)
+        total_students = len(students)
+        print("Total Students: "+str(total_students))
+
+        return redirect('/')
+    else:
+        return render(request, 'index.html', {'students':students})
